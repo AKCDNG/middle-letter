4ad05f521f8413337aa096459c63776a394959d4
Anish <anishkakaiya@outlook.com>
2022-06-21 11:40:13 +0100

    refactor code to distinguish even or odd words

--- middleLetter.js
+++ middleLetter.js
@@ -1,11 +1,15 @@
 class middleLetterFinder {
   middleLetter(word) {
     this.arr = word.split("");
-
-    if (word === 'whiteboard') {
-      return 'eb'
-    } else {
     this.letterIndex = (this.arr.length / 2);
+    
+    if (word.length % 2 === 0) {
+      this.letter1 = this.arr.at((this.letterIndex)- 1);
+      this.letter2 = this.arr.at(this.letterIndex)
+
+      this.totalLetters = `${this.letter1}${this.letter2}`
+      return this.totalLetters
+    } else {
     this.letter = this.arr.at(this.letterIndex)
     return this.letter
     }
